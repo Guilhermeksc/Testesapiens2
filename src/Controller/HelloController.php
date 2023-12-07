accf00885f68d7809873c089c308f12532d82d3f
Guilhermeksc <guilhermekscampos@gmail.com>
2023-12-06 21:48:26 -0300

    ajuste novo

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,7 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class HelloController
 {
-
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Hello World!</body></html>'
+        );
+    }
+    #[Route('/indexnovo')]
+    public function indexnovo(): Response
+    {
+        return new Response(
+            '<html><body>Novo index Hello World!</body></html>'
+        );
+    }
+    #[Route('/testeindex')]
+    public function testeindex(): Response
+    {
+        return new Response(
+            '<html><body>Teste index Hello World!</body></html>'
+        );
+    }
 }
